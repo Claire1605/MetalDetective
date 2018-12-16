7abee0881b0ca607bd06007b9f97494fa7cda898
jctwood <jamie.ct.wood@gmail.com>
2018-12-16 00:40:52 +0000

    Add Metalda mesh to scene

--- Metal Detective/Assets/Scripts/Player.cs
+++ Metal Detective/Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ public class Player : MonoBehaviour {
 
     public float speed = 1.0f;
     public float yFloat = 2.0f;
+	public Transform rotatePoint;
+	public Transform body;
+	public Transform aerial;
 
     private float x = 0.0f;
     private float y = 0.0f;
@@ -32,7 +35,20 @@ public class Player : MonoBehaviour {
 
         transform.Translate(movement * speed * Time.deltaTime);
 
-        Vector3 yAdjust = new Vector3(transform.position.x, y, transform.position.z);
+		if (movement.magnitude > 0.0f)
+		{
+			rotatePoint.transform.rotation = Quaternion.LookRotation(movement.normalized, Vector3.up);
+
+			body.transform.localEulerAngles = Vector3.Lerp(body.transform.localEulerAngles, new Vector3(movement.magnitude * 20.0f, 0, 0.0f), Time.deltaTime * 2.0f);
+			aerial.transform.localEulerAngles = Vector3.Lerp(aerial.transform.localEulerAngles, new Vector3(-movement.magnitude * 10.0f, 0, 0.0f), Time.deltaTime * 2.0f);
+		}
+		else
+		{
+			body.transform.localEulerAngles = Vector3.Lerp(body.transform.localEulerAngles, Vector3.zero, Time.deltaTime * 2.0f);
+			aerial.transform.localEulerAngles = Vector3.Lerp(aerial.transform.localEulerAngles, Vector3.zero, Time.deltaTime * 2.0f);
+		}
+
+		Vector3 yAdjust = new Vector3(transform.position.x, y, transform.position.z);
         transform.position =  Vector3.Lerp(transform.position, yAdjust, Time.deltaTime * 10.0f);
 	}
 }
